5ecb36954aa929b10d2aaf4bf8fd488d0c231014
Niels Witte <nielswitte@gmail.com>
2014-02-12 16:01:40 +0100

    Moved some functions to the controllers, where they belong. Renamed libs -> models Added documentatation.

---
+++ api/models/simpleModel.php
@@ -0,0 +1,26 @@
+<?php
+if(EXEC != 1) {
+	die('Invalid request');
+}
+
+/**
+ * Interface for most Models
+ * Includes some basic functions that need to be implemented
+ *
+ * @author Niels Witte
+ * @version 0.1
+ * @date February 11th, 2014
+ */
+interface SimpleModel {
+
+    function getInfoFromDatabase();
+
+    /**
+	 * Function to validate parameters array
+	 *
+	 * @param Array $parameters
+	 *
+	 * @return Boolean true when all checks passed
+	 */
+    static function validateParameters($parameters);
+}
