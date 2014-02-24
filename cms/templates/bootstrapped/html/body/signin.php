93f51d84f5231645a5e8f28a30cfb087a9f409b8
Niels Witte <nielswitte@gmail.com>
2014-02-24 14:41:18 +0100

    Fixed session handling when wrong username/password combination is used

--- cms/templates/bootstrapped/html/body/signin.php
+++ cms/templates/bootstrapped/html/body/signin.php
@@ -12,8 +12,19 @@ if(EXEC != 1) {
     } else {
         $text = 'previous page';
     }
+
+    if($isAuthorized) {
 ?>
 <div class="page-header">
     <h1>Signed In <small>Welcome</small></h1>
 </div>
 <p>You have been signed in. Click <a href="<?php echo $previousPage; ?>">here</a> to return to the <?php echo $text; ?>.</p>
+<?php
+    } else {
+?>
+<div class="page-header">
+    <h1>Sign in failed <small>wrong username/password combination</small></h1>
+</div>
+<p>You used a wrong username/password combination. Please try again.</p>
+<?php
+    }
