d5bc0f823a496f1edf436c298ee30d1af6c3253e
Niels Witte <nielswitte@gmail.com>
2014-02-24 14:41:31 +0100

    Added a not authorized page

---
+++ cms/templates/bootstrapped/html/body/notauthorized.php
@@ -0,0 +1,8 @@
+<div class="page-header">
+  <h1>Unauthorized <small>you need to be logged in</small></h1>
+</div>
+<p>
+    You need to log in with your account to access this page. Use the form in the top left corner to do so.
+    If you did logged in, it is possible that your session has expired. This can be the case when you've been
+    inactive for quite some time. Again, you need to log in to view this page.
+</p>
