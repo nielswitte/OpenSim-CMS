1bca0095a91c9560227e686b484e9bc3915b0b8c
Niels Witte <nielswitte@gmail.com>
2014-02-26 11:22:07 +0100

    Only loading the required events, so no new request is made every time the user switches week

--- cms/templates/bootstrapped/js/meetings.js
+++ cms/templates/bootstrapped/js/meetings.js
@@ -1,28 +1,29 @@
 jQuery(document).ready(function($) {
     // Get date two weeks back (1000 miliseconds, 60 seconds, 60 minutes, 24 hours, 14 days)
     var date = new Date(new Date - (1000*60*60*24*14));
-
-    $('#calendar').fullCalendar({
-        defaultView: 'agendaWeek',
-        height: 650,
-        events: {
-            // Get all meetings from the past two weeks and the future. Month+1 because getMonth ranges from 0 to 11.
-            url: base_url +'/api/meetings/'+ date.getFullYear() +'-'+ (date.getMonth()+1) +'-'+ date.getDate() +'/calendar/?token='+ api_token
-        },
-        eventClick: function(event) {
-            // Create a pop over for this event
-            $(this).popover({
-                placement: 'auto right',
-                title: event.title,
-                content: event.description
-            });
-            // Imediatly show it
-            $(this).popover('show');
-            return false;
-        },
-        loading: function(bool) {
-            if (bool) $('#loading').show();
-            else $('#loading').hide();
-        }
+    $('#loading').show();
+    // Get meetings
+    client.meetings.read(date.getFullYear() +'-'+ (date.getMonth()+1) +'-'+ date.getDate() +'/calendar/', { token: api_token }).done(function(data) {
+        // Insert the events into the calendar
+        $('#calendar').fullCalendar({
+            defaultView: 'agendaWeek',
+            height: 650,
+            events: data,
+            eventClick: function(event) {
+                // Create a pop over for this event
+                $(this).popover({
+                    placement: 'auto right',
+                    title: event.title,
+                    content: event.description
+                });
+                // Imediatly show it
+                $(this).popover('show');
+                return false;
+            }
+        });
+        $('#loading').hide();
+    }).fail(function() {
+        addAlert('danger', '<strong>Error!</strong> Loading the calendar events failed.');
+        $('#loading').hide();
     });
 });
