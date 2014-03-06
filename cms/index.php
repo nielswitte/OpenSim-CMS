12b9e3bdc473eeac1e845f398412a16cd5a080a6
Niels Witte <nielswitte@gmail.com>
2014-03-06 12:22:44 +0100

    Fixed rewrite rules and unused PHP. Almost all PHP is removed from the client now

--- cms/index.php
+++ cms/index.php
@@ -1,19 +1,4 @@
 <?php
 require dirname(__FILE__) .'/../config.php';
-
-session_start();
-$sessionExpired = FALSE;
-
-if(isset($_SESSION['AccessTokenExpires'])) {
-    if($_SESSION['AccessTokenExpires'] < date('Y-m-d H:i:s')) {
-        unset($_SESSION);
-        session_destroy();
-        // @todo nice implementation
-        $sessionExpired = TRUE;
-    } else {
-        $_SESSION['AccessTokenExpires'] = date('Y-m-d H:i:s', strtotime('+'. SERVER_API_TOKEN_EXPIRES));
-    }
-}
-
 require dirname(__FILE__) .'/templates/restangular/index.php';
 
