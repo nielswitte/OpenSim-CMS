58b0c20585fb257c6b5fd27e9831833250c8046d
Niels Witte <nielswitte@gmail.com>
2014-03-06 14:22:55 +0100

    Fixed the helper to parse JSON objects

--- includes/class.Helper.php
+++ includes/class.Helper.php
@@ -76,38 +76,45 @@ class Helper {
      * @return array
      */
     public static function parsePutRequest($input) {
-        // grab multipart boundary from content type header
-        preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
-        // Is multipart form?
-        if(isset($matches[1])) {
-            $boundary = $matches[1];
-
-            // split content by boundary and get rid of last -- element
-            $a_blocks = preg_split("/-+$boundary/", $input);
-            array_pop($a_blocks);
-
-            // loop data blocks
-            foreach ($a_blocks as $id => $block) {
-                if (empty($block))
-                    continue;
-
-                // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
-                // parse uploaded files
-                if (strpos($block, 'application/octet-stream') !== FALSE) {
-                    // match "name", then everything after "stream" (optional) except for prepending newlines
-                    preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
-                }
-                // parse all other fields
-                else {
-                    // match "name" and optional value in between newline sequences
-                    preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
-                }
-                $a_data[$matches[1]] = $matches[2];
-            }
-        // Try to parse as key value pairs
+        // Parse JSON
+        if(substr($input, 0, 1) == "{" && substr($input, -1) == "}") {
+            $a_data = json_decode($input, TRUE);
+        // Parse other form types
         } else {
-            parse_str($input, $a_data);
+            // grab multipart boundary from content type header
+            preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+            // Is multipart form?
+            if(isset($matches[1])) {
+                $boundary = $matches[1];
+
+                // split content by boundary and get rid of last -- element
+                $a_blocks = preg_split("/-+$boundary/", $input);
+                array_pop($a_blocks);
+
+                // loop data blocks
+                foreach ($a_blocks as $id => $block) {
+                    if (empty($block))
+                        continue;
+
+                    // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
+                    // parse uploaded files
+                    if (strpos($block, 'application/octet-stream') !== FALSE) {
+                        // match "name", then everything after "stream" (optional) except for prepending newlines
+                        preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+                    }
+                    // parse all other fields
+                    else {
+                        // match "name" and optional value in between newline sequences
+                        preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+                    }
+                    $a_data[$matches[1]] = $matches[2];
+                }
+            // Try to parse as key value pairs
+            } else {
+                parse_str($input, $a_data);
+            }
         }
+
         return $a_data;
     }
 }
