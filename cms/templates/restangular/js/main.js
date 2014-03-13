04052a3593796ccafc2dfb9128ba9d7434c64e5b
Niels Witte <nielswitte@gmail.com>
2014-03-13 09:49:27 +0100

    CMS: Added additional comments to constants to make it easier to look up a value

--- cms/templates/restangular/js/main.js
+++ cms/templates/restangular/js/main.js
@@ -1,13 +1,13 @@
 var partial_path = base_url +'/cms/templates/restangular/html/partials';
 var controller_path = base_url +'/cms/templates/restangular/js/controllers';
 
-/** @const No permissions */
+/** @const No (0) permissions */
 var NONE    = 0;
-/** @const Read permissions */
+/** @const Read (4) permissions */
 var READ    = 4;
-/** @const Execute permissions */
+/** @const Execute (5) permissions */
 var EXECUTE = 5;
-/** @const Write permissions */
+/** @const Write (6) permissions */
 var WRITE   = 6;
-/** @const All permissions */
+/** @const All (7) permissions */
 var ALL     = 7;
