58d0e1bdc7a74c3f182b4e26f61cbc84d4ee8858
Niels Witte <nielswitte@gmail.com>
2014-04-07 11:11:25 +0200

    API: Allow searching for . (dots) in filenames and usernames

--- api/modules/user.php
+++ api/modules/user.php
@@ -40,7 +40,7 @@ class User extends Module {
     public function setRoutes() {
         $this->api->addRoute("/^\/users\/?$/",                                          'getUsers',                 $this, 'GET',    \Auth::READ);     // Gets a list of all users
         $this->api->addRoute("/^\/users\/(\d+)\/?$/",                                   'getUsers',                 $this, 'GET',    \Auth::READ);     // Gets a list of all users starting at the given offset
-        $this->api->addRoute("/^\/users\/([a-zA-Z0-9-_]{3,}+)\/?$/",                    'getUsersByUsername',       $this, 'GET',    \Auth::READ);     // Gets a list of all users with usernames matching the search of atleast 3 characters
+        $this->api->addRoute("/^\/users\/([a-zA-Z0-9-_\.]{3,}+)\/?$/",                  'getUsersByUsername',       $this, 'GET',    \Auth::READ);     // Gets a list of all users with usernames matching the search of atleast 3 characters
         $this->api->addRoute("/^\/user\/?$/",                                           'createUser',               $this, 'POST',   \Auth::WRITE);    // Create a new CMS user
         $this->api->addRoute("/^\/user\/(\d+)\/?$/",                                    'getUserById',              $this, 'GET',    \Auth::READ);     // Get a user by ID
         $this->api->addRoute("/^\/user\/(\d+)\/?$/",                                    'updateUserById',           $this, 'PUT',    \Auth::READ);     // Update the given user
