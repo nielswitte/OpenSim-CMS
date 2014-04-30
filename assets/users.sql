3a39cd4cb166a63056ecb4c8f53211860e7fe78b
Niels Witte <nielswitte@gmail.com>
2014-04-30 10:06:47 +0200

    Assets: Fixed user query

--- assets/users.sql
+++ assets/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `email`, `password`, `lastLogin`) VALUES
-	(1, 'OpenSim', 'Open', 'Sim', NULL, '$2y$10$6Ypy3i9luoc01qUTi3wnCe9SPiOYVWdVO0GgiPXpN.Mc3V.wjMlfS', NULL),
+	(1, 'OpenSim', 'Open', 'Sim', 'root@localhost', '$2y$10$6Ypy3i9luoc01qUTi3wnCe9SPiOYVWdVO0GgiPXpN.Mc3V.wjMlfS', NULL),
 	(2, 'admin', 'John', 'Doe', 'john@doe.com0', '$2y$10$chCX.iTGaLF7v/2aQY8qZe3.BGhguBTcuqXupCL/XrN32PC5Ee9pq', NULL);
 
 INSERT INTO `user_permissions` (`userId`, `auth`, `chat`, `comment`, `document`, `file`, `grid`, `meeting`, `meetingroom`, `presentation`, `user`) VALUES
